4eaa1aad87fc5d59da247b99244eee8e55e15cec
Amit1494 <146162073+Amit1494@users.noreply.github.com>
2026-03-26 19:16:49 +0530

    Sync all changes

--- src/main/java/com/product/catalog/controller/ProductController.java
+++ src/main/java/com/product/catalog/controller/ProductController.java
@@ -2,16 +2,13 @@ package com.product.catalog.controller;
 
 import com.product.catalog.model.Product;
 import com.product.catalog.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-
+@CrossOrigin(origins ="http://localhost:5173")
 public class ProductController {
     private final ProductService productService;
 
